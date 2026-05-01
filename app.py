ab52ce670bc141e67a55da0c9010961b9ae7487a
Nishtha Megha <nishthamegha@gmail.com>
2026-05-01 19:56:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
-from keras.models import load_model
-from PIL import Image
+from tensorflow.keras.models import load_modelfrom PIL import Image
 import os
 
 # Hide TF warnings
